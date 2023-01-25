e8676d88dc681e15912e11288fe71ad4fbed850f
Rudy Matela <rudy@matela.com.br>
2023-01-25 09:29:11 +0100

    add doctests

--- python/src/reverseno.py
+++ python/src/reverseno.py
@@ -1,9 +1,23 @@
 
 
 def reverse(n):
+    """
+    This function reverses the digits of a given integer.
+
+    >>> reverse(234)
+    432
+
+    >>> reverse(123)
+    321
+    """
     r = 0
     while n > 0:
         r = r * 10 + n % 10
         n = n // 10
     return r
 
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
