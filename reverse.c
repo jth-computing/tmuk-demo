a24c75877d3866495e102123644c86e2ffc415f7
Rudy Matela <rudy@matela.com.br>
2023-02-08 10:25:25 +0100

    remove cruft

--- reverse.c
+++ reverse.c
@@ -4,12 +4,9 @@
 int main()
 {
 	int x;
-	printf("Begin\n");
 	printf("Please type an integer: ");
 	scanf("%d", &x);
 	printf("Result: ");
 	printf("%d\n", reverse(x));
-	printf("end.\n");
-	printf("real end end.\n");
 	return 0;
 }
